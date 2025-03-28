f9812fe58f8e93d002a09f6f9d67c4a6f35f67f3
kazishakkhar17 <kazishakkhar04@gmail.com>
2025-03-28 08:50:17 +0600

    Test frontend initialization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org"  //test comment
           target="_blank"
           rel="noopener noreferrer"
         >
